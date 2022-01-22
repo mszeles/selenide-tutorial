1c3e4f56b09b7445813ba2469e6602176b455497
MSzeles <miklos.szeles@dmgmori-heitec.hu>
2022-01-22 16:39:17 +0100

    Extracting elements as constants Chaining verifications

--- src/test/java/com/mszeles/selenide_tutorial/stackowerflow/HomepageTest.java
+++ src/test/java/com/mszeles/selenide_tutorial/stackowerflow/HomepageTest.java
@@ -1,5 +1,6 @@
 package com.mszeles.selenide_tutorial.stackowerflow;
 
+import com.codeborne.selenide.SelenideElement;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.enabled;
@@ -8,12 +9,13 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class HomepageTest {
 
+    private final SelenideElement loginButton = $x("(//a[contains(@class, 'login-link')])[1]");
+    private final SelenideElement signUpButton = $x("(//a[contains(@class, 'login-link')])[2]");
+
     @Test
     public void openPage() {
         open("https://stackoverflow.com/");
-        $x("(//a[contains(@class, 'login-link')])[1]").shouldBe(visible);
-        $x("(//a[contains(@class, 'login-link')])[1]").shouldBe(enabled);
-        $x("(//a[contains(@class, 'login-link')])[2]").shouldBe(visible);
-        $x("(//a[contains(@class, 'login-link')])[2]").shouldBe(enabled);
+        loginButton.shouldBe(visible).shouldBe(enabled);
+        signUpButton.shouldBe(visible).shouldBe(enabled);
     }
 }
